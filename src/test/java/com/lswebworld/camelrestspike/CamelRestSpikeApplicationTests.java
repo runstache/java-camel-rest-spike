1a3537e293902c1408999692dd9c1d0615378b9f
Larry Shields <shieldsl@upmc.edu>
2019-05-20 14:50:25 -0400

    Project Setup

---
+++ src/test/java/com/lswebworld/camelrestspike/CamelRestSpikeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lswebworld.camelrestspike;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CamelRestSpikeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
